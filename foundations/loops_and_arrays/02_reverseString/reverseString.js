c86ec7161637c652f7108a5d532f048a2787d6c5
Azkal Alghani <Alghani175@gmail.com>
2026-09-25 19:00:00 +0700

    exercise 02 - reverse a string
    done perfectly

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(statement) {
+    let result = '';
+    for (let i = statement.length - 1; i >= 0; i--) {
+        result += statement[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
