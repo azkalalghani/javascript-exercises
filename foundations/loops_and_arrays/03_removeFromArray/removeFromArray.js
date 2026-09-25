37a28d087473e356e466f61ebc3be217bc1432d1
Azkal Alghani <Alghani175@gmail.com>
2026-09-25 19:15:54 +0700

    Exercise 03 - removeFromArray
    done perfectly

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...target) {
+    return arr.filter((item) => !target.includes(item));
 };
 
 // Do not edit below this line
