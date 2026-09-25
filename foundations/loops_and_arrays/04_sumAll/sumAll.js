77f51dfe096029d017b69321f7fc928733efe641
Azkal Alghani <Alghani175@gmail.com>
2026-09-25 20:54:00 +0700

    Exercise 04 - sumAll
    Setting invalid filter for negative numbers, non-integers, strings and return error

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(initial, final) {
+    if (initial < 0 || final < 0 ||
+        !Number.isInteger(initial) || !Number.isInteger(final)) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    if (initial > final) {
+        for (let i = final; i <= initial; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = initial; i <= final; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
