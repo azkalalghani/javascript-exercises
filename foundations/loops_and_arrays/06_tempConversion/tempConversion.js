3df323516d6d0e511caf0a14825a91be583f88de
Azkal Alghani <Alghani175@gmail.com>
2026-09-25 21:38:59 +0700

    exercice 06 - tempConversion
    perfectly done by adding (formula * 10) /10 so it returns the perfect integer we want

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  const formula = (degrees - 32) * 5/9;
+  return Math.round(formula * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  const formula = (degrees * 9/5) + 32;
+  return Math.round(formula * 10) / 10;
 };
 
 // Do not edit below this line
